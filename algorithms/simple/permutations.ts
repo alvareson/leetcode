ea929d9bdcea166cadfdaab6931d3bb1eb32dad6
Alister <a.enikeev@mirador.software>
2024-05-20 02:19:42 +0400

    permutation task

--- algorithms/simple/permutations.ts
+++ algorithms/simple/permutations.ts
@@ -1,7 +1,20 @@
 const permute = (nums: number[]): number[][] => {
-    return nums.flatMap((n) => {
-        return permute(nums.filter(i => i !== n)).map(i => [n, ...i])
-    })
+    const result: number[][] = []
+    const myPermute = (start: number[], permutations: number[]) => {
+        if (start.length === 0) {
+            result.push([...permutations])
+        } else {
+            for (let i = 0; i < start.length; i++) {
+                let dupe = [...start]
+                let removed = dupe.splice(i, 1)[0]
+                permutations.push(removed)
+                myPermute(dupe, permutations)
+                permutations.pop()
+            }
+        }
+    }
+    myPermute(nums, [])
+    return result
 }
 
 console.log(permute([1,2,3]))
