d8c0126ae5088c215992dd9a20591c36598b6f9f
Alister <a.enikeev@mirador.software>
2024-09-17 00:31:48 +0400

    solved

--- typescript/core/medium/min_time_difference_24clock.ts
+++ typescript/core/medium/min_time_difference_24clock.ts
@@ -1,14 +1,22 @@
 function findMinDifference(timePoints: string[]): number {
   const convertToMinutes = (time: string): number => {
-    const [hours, minutes] = time.split(':').map(Number);
-    return hours * 60 + minutes;
+    const [hours, minutes] = time.split(':').map(Number)
+    return hours * 60 + minutes
   }
 
   const minutesArray = timePoints.map(convertToMinutes)
   minutesArray.sort((a, b) => a - b)
   console.log(minutesArray)
+  
+  let minDifference = Number.MAX_SAFE_INTEGER
+  for (let i = 1; i < minutesArray.length; i++) {
+    minDifference = Math.min(minDifference, minutesArray[i] - minutesArray[i - 1])
+  }
 
-  return 0
+  const circularDifference = 1440 - (minutesArray[minutesArray.length - 1] - minutesArray[0])
+  minDifference = Math.min(minDifference, circularDifference)
+
+  return minDifference
 }
 
 console.log(findMinDifference(["23:59","00:00"]))
