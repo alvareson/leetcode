0440f84f7d3a25d64e9d598b6fabef256a374a9f
Alister <a.enikeev@mirador.software>
2024-09-22 23:49:01 +0400

    solved

---
+++ typescript/core/hard/kth_smallest_in_lexigraphical_order.ts
@@ -0,0 +1,28 @@
+function findKthNumber(n: number, k: number): number {
+  function calculateSteps(n: number, n1: number, n2: number): number {
+    let steps = 0
+    while (n1 <= n) {
+      steps += Math.min(n + 1, n2) - n1
+      n1 *= 10
+      n2 *= 10
+    }
+    return steps
+  }
+
+  let curr = 1
+  k--
+  while (k > 0) {
+    let steps = calculateSteps(n, curr, curr + 1)
+    if (steps <= k) {
+      curr++
+      k -= steps
+    } else {
+      curr *= 10
+      k--
+    }
+  }
+  return curr
+    
+}
+
+console.log(findKthNumber(1, 1))
