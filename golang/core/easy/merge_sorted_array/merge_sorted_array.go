e6fe013a408cce588e7e6b551387a32338b5f472
Alister <a.enikeev@mirador.software>
2025-01-03 11:17:22 +0400

    feat golang easy/merge sorted array kata

--- golang/core/easy/merge_sorted_array/merge_sorted_array.go
+++ golang/core/easy/merge_sorted_array/merge_sorted_array.go
@@ -1,11 +1,24 @@
 package main
 
-func merge([]int nums1, int m, []int nums2, int n) void {
+import (
+  "sort"
+)
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
   temp := make([]int, 0)
-  for i
+  for i := 0; i < m; i++ {
+    temp = append(temp, nums1[i])
+  }
+  for i := 0; i < n; i++ {
+    temp = append(temp, nums2[i])
+  }
+  sort.Ints(temp)
+  for i := 0; i < m + n; i++ {
+    nums1[i] = temp[i]
+  }
 }
 
 
 func main() {
-  
+  merge([]int{1,2,3,0,0,0}, 3, []int{2,5,6}, 3)
 }
