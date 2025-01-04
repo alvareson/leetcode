feeda732ad9e50d6223d4a2bd1f217969bfda56c
Alister <a.enikeev@mirador.software>
2025-01-04 11:00:20 +0400

    solved

---
+++ golang/core/medium/unique_length_3_palindromic_subsequences/unique_length_3_palindromic_subsequences.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func countPalindromicSubsequence(s string) int {
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j <
+	}
+  return 0
+}
+
+func main() {
+	fmt.Println(countPalindromicSubsequence("aabca"))
+}
