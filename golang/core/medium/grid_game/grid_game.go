1782e0042b2c508dd58b5930138b85d32af113e7
Alister <a.enikeev@mirador.software>
2025-01-21 20:10:03 +0400

    solved

--- golang/core/medium/grid_game/grid_game.go
+++ golang/core/medium/grid_game/grid_game.go
@@ -1,9 +1,49 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math"
+)
+
+func fillPrefixSum(nums []int) []int {
+	prefixSum := make([]int, len(nums))
+	prefixSum[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		prefixSum[i] = prefixSum[i-1] + nums[i]
+	}
+	return prefixSum
+}
 
 func gridGame(grid [][]int) int64 {
-  return 0
+  n := len(grid[0])
+  prefixTop := fillPrefixSum(grid[0])
+  prefixBottom := fillPrefixSum(grid[1])
+
+  totalTop := prefixTop[n-1]
+
+  ans := int64(math.MaxInt64)
+  for i := 0; i < n; i++ {
+    // topScore - score 2nd robot collects from top row if dropped to 2nd row at i-th col
+		topScore := int64(totalTop)
+		topScore = int64(totalTop - prefixTop[i])
+    fmt.Println(topScore)
+
+    // bottomScore - score 2nd robot collects from bottom row if dropped to 2nd row at i-th col
+		bottomScore := int64(0)
+		if i > 0 {
+			bottomScore = int64(prefixBottom[i-1])
+		}
+
+		secondRobotScore := topScore
+		if bottomScore > secondRobotScore {
+			secondRobotScore = bottomScore
+		}
+
+		if secondRobotScore < int64(ans) {
+			ans = secondRobotScore
+		}
+  }
+	return ans
 }
 
 func main() {
